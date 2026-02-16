da2ed1d50a5e405a2fbf6ed042ae57c439f58dab
Alejandro Ramirez <alejandrocohete@gmail.com>
2026-02-16 12:26:47 -0400

    Update urls

--- url_shortener/shortener/urls.py
+++ url_shortener/shortener/urls.py
@@ -1,7 +1,8 @@
 from .forms import URLForm
-from .views import shorten_url
+from .views import shorten_url, shorten_url_redirect
 from django.urls import path
 
 urlpatterns = [
 	path('', shorten_url, name='shorten_url'),
+	path('<str:short_code>/', shorten_url_redirect, name='shorten_url_redirect'),
 ]
