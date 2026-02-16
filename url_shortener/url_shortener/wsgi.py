88040d14e4af6bd885f6d96780c37dfb7eb2a35b
Alejandro Ramirez <alejandrocohete@gmail.com>
2026-02-16 11:44:11 -0400

    Update wsgi

--- url_shortener/url_shortener/wsgi.py
+++ url_shortener/url_shortener/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for url_shortener project.
+WSGI project for url_shortener.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Agrega la carpeta url_shortener al sys.path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'url_shortener.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
